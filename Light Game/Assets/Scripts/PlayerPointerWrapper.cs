4bc35f1da951c7acb14d6683158d1b08618f3a73
Galen A <gatkins2@uccs.edu>
2020-02-26 17:58:19 -0700

    Menus

---
+++ Light Game/Assets/Scripts/PlayerPointerWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPointerWrapper : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Point from player
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
